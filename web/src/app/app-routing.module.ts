c0f95691b918a77f2bc121c292c1dde012a4441f
Yuepeng Chen <chen.7099@osu.edu>
2019-04-14 20:52:48 -0400

    Sunday

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CustomersComponent } from './customers/customers/customers.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'customers', component: CustomersComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,22 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { CustomersComponent } from './customers/customers/customers.component';
 import { CustomerRowComponent } from './customers/customer-row/customer-row.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import {FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     CustomersComponent,
-    CustomerRowComponent
+    CustomerRowComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
